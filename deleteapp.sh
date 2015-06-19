1eaabdec38208ead82b35c3d429b2f18c0574053
Christian Posta <christian.posta@gmail.com>
2015-06-19 08:16:37 -0700

    use gerrit 2.3-SNAPSHOT

--- deleteapp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-osc get rc | grep fabric8 | awk '{print $1}' | xargs osc delete rc {}
-osc get service | grep fabric8 | awk '{print $1}' | xargs osc delete service {}
--- deletefabric8.sh
+++ deletefabric8.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 oc delete all -l provider=fabric8
+. ./deleteroutes.sh
