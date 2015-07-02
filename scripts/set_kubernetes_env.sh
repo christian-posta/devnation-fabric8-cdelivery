dd2eae59fe1388beb0fc6f7c40a6cc6af4ce44f9
Charles Moulliard <ch007m@gmail.com>
2015-07-02 19:36:22 +0200

    Use vagrant.f8 as hostname as latest vagrant image proposes a DNS server

--- scripts/set_kubernetes_env.sh
+++ scripts/set_kubernetes_env.sh
@@ -2,7 +2,7 @@
 
 unset DOCKER_CERT_PATH
 unset DOCKER_TLS_VERIFY
-export DOCKER_HOST=tcp://vagrant.local:2375
+export DOCKER_HOST=tcp://vagrant.f8:2375
 export KUBERNETES_NAMESPACE=default
 export KUBERNETES_MASTER=https://172.28.128.4:8443
 export KUBERNETES_DOMAIN=vagrant.local
